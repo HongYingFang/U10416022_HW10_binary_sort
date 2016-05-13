a62b8e244db5a7407fa346ac2310cb2eabe269a4
HongYingFang <zx04785@gmail.com>
2016-05-13 22:06:25 +0800

    Create TestBinaryTree.java
    insert ten numbers to test this program (put numbers in sequence) and check whether the node value exists

---
+++ TestBinaryTree.java
@@ -0,0 +1,26 @@
+public class TestBinaryTree {
+	public static void main(String arg[]) {
+		BinaryTree b = new BinaryTree();
+		//insert ten numbers(element)
+		b.insert(3);
+		b.insert(7);
+		b.insert(15);
+		b.insert(34);
+		b.insert(58);
+		b.insert(70);
+		b.insert(65);
+		b.insert(2);
+		b.insert(77);
+		b.insert(34);
+		//print out result
+		System.out.println("Original Tree : ");
+		//call display method and put the numbers in sequence by binary tree
+		b.display(BinaryTree.root);
+		//call search method to check whether the node exists or not
+		System.out.println("");
+		System.out.println("Check whether the node value 4 exists : " + b.search(4));
+		System.out.println("Check whether the node value 7 exists : " + b.search(7));
+		
+	}
+
+}
